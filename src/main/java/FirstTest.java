e61bd302326527d7159f168a5c6cd84fe0e46c79
andrey773022@gmail.com <BeBeViSnSiS7845>
2020-11-08 15:56:42 +0300

    kllllll

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -1,6 +1,12 @@
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.Set;
 
 public class FirstTest {
 
@@ -9,8 +15,19 @@ public class FirstTest {
        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://google.com");
+        driver.get("https://google.com/222");
+//        WebElement searchBox = driver.findElement(By.name("q"));
+//        searchBox.sendKeys("ChromeDriver");
+//        searchBox.submit();
+//        //driver.quit();
 
+        Set<String> allLogsType =  driver.manage().logs().getAvailableLogTypes();
+        System.out.println("allLogsType ->" + allLogsType);
 
+        LogEntries logs = driver.manage().logs().get(LogType.BROWSER);
+        for (
+                LogEntry entry: logs
+        )
+        System.out.println("logs -> " + entry);
     }
 }
